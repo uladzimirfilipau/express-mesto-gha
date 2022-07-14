ea28a33c02f1534951294856c4e73be6f5f497ae
Vova <uladzimirfilipau@gmail.com>
2022-07-14 10:29:08 +0300

    feat: add app.js

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const mongoDB = "mongodb://localhost:27017/mestodb";
+
+const { PORT = 3000 } = process.env;
+const app = express();
+
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, "public")));
+
+mongoose.connect(mongoDB, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+
+app.listen(PORT);
